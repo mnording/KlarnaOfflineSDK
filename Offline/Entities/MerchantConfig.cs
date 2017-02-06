2429a5a3f2390f3dbcf63574a970a147c58ddc48
Mattias Nording <mattiasnording@gmail.com>
2017-02-06 13:50:51 +0100

    Updating constructs

--- Offline/Entities/MerchantConfig.cs
+++ Offline/Entities/MerchantConfig.cs
@@ -23,15 +23,36 @@ namespace Klarna.Offline.Entities
         /// <param name="server">What server is being targeted?</param>
         public MerchantConfig(CultureInfo locale, string currency, string country, string sharedSecret,string merchantId,Server server)
         {
-           VerifyCountries(currency,country);
+            SetCommonParams(locale,currency,country,sharedSecret,merchantId,server);
+        }
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="locale">The locale of the purchase</param>
+        /// <param name="currency">The currency of the purchase ISO 4217</param>
+        /// <param name="country">2 letter country code ISO 3166-1 alpha-2</param>
+        /// <param name="sharedSecret">The sharedsecret between you and Klarna</param>
+        /// <param name="merchantId">The merchant ID provided by Klarna</param>
+        /// <param name="server">What server is being targeted?</param>
+        public MerchantConfig(CultureInfo culture, string sharedSecret, string merchantId, Server server)
+        {
+            var region = new RegionInfo(culture.LCID);
+            var country = region.TwoLetterISORegionName;
+            var currency = region.ISOCurrencySymbol;
+            SetCommonParams(culture, currency, country, sharedSecret, merchantId, server);
+        }
+
+        private void SetCommonParams(CultureInfo locale, string currency, string country, string sharedSecret, string merchantId, Server server)
+        {
+            VerifyCountries(currency, country);
             this.locale = locale.Name;
             purchaseCountry = country;
             purchaseCurrency = currency;
             this.merchantId = merchantId;
             this.sharedSecret = sharedSecret;
             this.server = server;
-        }
 
+        }
         private void VerifyCountries(string currency, string country)
         {
             if (currency == null) throw new ArgumentNullException(nameof(currency));
